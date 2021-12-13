c5bbf6dade53961474e069de6bc98bca1aa89f4f
Heroszhen <zhen.yang@hotmail.fr>
2021-12-13 12:00:04 +0100

    maj

--- app/Routes.php
+++ app/Routes.php
@@ -1,10 +1,8 @@
 <?php
-
-use App\Controller\SecurityController;
 use FastRoute\RouteCollector;
 
 
 return function(RouteCollector $r) {
-    $r->addRoute('GET', '/',array(new App\Controller\HomeController($_GET["twig"]), "index",[]));
+    $r->addRoute('GET', '/',array(new App\Controller\ExempleHomeController(), "index",[]));
     
 };
--- src/Controller/ExempleHomeController.php
+++ src/Controller/ExempleHomeController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use Frameworkphp3wa\AbstractController;
 use Frameworkphp3wa\FlashBag;
 
-class HomeController extends AbstractController{
+class ExempleHomeController extends AbstractController{
 
     public function index(){
         $flash = new FlashBag();
         $flash->empty();
         $flash->set("exemple flashbag message","info");
-        return $this->render("home.index.twig",[
+        return $this->render("exemplehome.index.twig",[
             "flash" => $flash->get()
         ]);
     }
