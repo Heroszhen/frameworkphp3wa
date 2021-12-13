69b58f27ea0ce6c59ee696fee0d725f08f6edffb
Heroszhen <zhen.yang@hotmail.fr>
2021-12-13 13:08:45 +0100

    maj

--- app/Routes.php
+++ app/Routes.php
@@ -1,7 +1,6 @@
 <?php
 use FastRoute\RouteCollector;
 
-
 return function(RouteCollector $r) {
     $r->addRoute('GET', '/{id:\d+}',array(new App\Controller\ExempleHomeController(), "index"));
     
